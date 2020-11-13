93c48cbdd75f0e4666eab07a2d104dc0c3889e2b
Josef Ondrej <josef.ondrej@outlook.com>
2020-11-13 12:57:53 +0100

    Add example on how to estimate multilevel glm in statsmodels

---
+++ multilevel_glm_in_python/estimate_multilevel_glm_in_statsmodels.py
@@ -0,0 +1,21 @@
+# How to estimate multilevel GLM in statsmodels package
+# Prerequisite for this script is running generate_data.py
+
+# We only show example for gaussian model, because gamma model is not implemented in the package
+# see: https://www.statsmodels.org/devel/mixed_glm.html
+
+from pandas import read_csv
+from statsmodels.regression.mixed_linear_model import MixedLM
+
+gaussian_data = read_csv("/tmp/multilevel-glm-in-python/Gaussian_identity_data.csv")
+
+model = MixedLM.from_formula("y ~ x1 + x2 + x3", data=gaussian_data, groups=gaussian_data["group_index"])
+
+# Fixed effects
+model_result = model.fit()
+
+# Random effects
+print(model_result.random_effects)
+
+# Dispersion parameter
+print(model_result.scale)
